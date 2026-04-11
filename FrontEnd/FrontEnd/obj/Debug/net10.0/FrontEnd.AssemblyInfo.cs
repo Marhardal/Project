f22a105df0c72563d6bcdcd438ef6b8f9ac4e8e4
Martin Harawa <martindharawa@gmail.com>
2026-04-11 15:36:17 +0200

    Added all the components needed.

--- FrontEnd/FrontEnd/obj/Debug/net10.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/FrontEnd/obj/Debug/net10.0/FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f0edf37cd3f10c24b5eead0afa710cad707997")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ca6997af795ada13ea33b9db91e5966d03e6d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

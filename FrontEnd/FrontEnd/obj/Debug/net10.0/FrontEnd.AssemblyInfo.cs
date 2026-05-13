5fbf8c327535c6151791151e4b8166c43ff10bbf
Martin Harawa <martindharawa@gmail.com>
2026-05-13 14:05:43 +0200

    Added Icons to the dashcard.

--- FrontEnd/FrontEnd/obj/Debug/net10.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/FrontEnd/obj/Debug/net10.0/FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98f18c653b3d5ade14946179dd54fd5cec4d64b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6db4b745617da2a05d8187bc968e632182498998")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

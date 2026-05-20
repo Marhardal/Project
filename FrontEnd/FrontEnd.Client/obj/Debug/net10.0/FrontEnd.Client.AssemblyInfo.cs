c7dbb6a3ab6be47e83614295eeb99ccb9ea25b7d
Martin Harawa <martindharawa@gmail.com>
2026-05-20 19:20:05 +0200

    Usercan now successfully login and navigation bar opens.

--- FrontEnd/FrontEnd.Client/obj/Debug/net10.0/FrontEnd.Client.AssemblyInfo.cs
+++ FrontEnd/FrontEnd.Client/obj/Debug/net10.0/FrontEnd.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a398288cbdc38d9be4c9c55dc3b387dbe6d546")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e864bc187a3f9a554066a82302f13d783c4c73")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

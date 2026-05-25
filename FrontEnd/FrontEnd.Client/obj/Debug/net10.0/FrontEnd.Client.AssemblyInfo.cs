4d179c6bef00d38b7c594ce1dfb37dca76dc86db
Martin Harawa <martindharawa@gmail.com>
2026-05-25 08:23:42 +0200

    Cleaning.

--- FrontEnd/FrontEnd.Client/obj/Debug/net10.0/FrontEnd.Client.AssemblyInfo.cs
+++ FrontEnd/FrontEnd.Client/obj/Debug/net10.0/FrontEnd.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd8bbd5d4ed0d740a9a7afd84cbc95be08d6eaa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13babebda4e089d64a590a1c7938749b9a49bf32")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

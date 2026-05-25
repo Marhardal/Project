d0037e9547afa218a9ae6b38194fb919b72afc71
Martin Harawa <martindharawa@gmail.com>
2026-05-25 11:31:44 +0200

    Status modal opens and user can select color but remaining with saving to database.

--- FrontEnd/FrontEnd/obj/Debug/net10.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/FrontEnd/obj/Debug/net10.0/FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13babebda4e089d64a590a1c7938749b9a49bf32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d179c6bef00d38b7c594ce1dfb37dca76dc86db")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

8ba7fae3d88c7127b2e16eadb7e0424a88d54010
Martin Harawa <martindharawa@gmail.com>
2026-05-26 20:43:49 +0200

    c

--- FrontEnd/FrontEnd/obj/Debug/net10.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/FrontEnd/obj/Debug/net10.0/FrontEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0037e9547afa218a9ae6b38194fb919b72afc71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753326d88b5a02781e3963045161200be8017e5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

3ad6a08db60d0e508407e90d0fa86afced0d8103
Martin Harawa <martindharawa@gmail.com>
2026-06-08 15:14:53 +0200

    When Hovering a district the card appears even outside the map card. Changed status from Approved to collected.

--- FrontEnd/FrontEnd.Client/obj/Debug/net10.0/FrontEnd.Client.AssemblyInfo.cs
+++ FrontEnd/FrontEnd.Client/obj/Debug/net10.0/FrontEnd.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25bd6a16bca79e79024c24ee759501fdb847e80a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2356f6bbe22d343ce32fd8c7cfb8e88c9d05e3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

535cdf8d376b7f93ea38b6f30798d302d44392dd
Martin Harawa <martindharawa@gmail.com>
2026-06-13 20:07:15 +0200

    User actions are limited on each page.

--- FrontEnd/FrontEnd.Client/obj/Debug/net10.0/FrontEnd.Client.AssemblyInfo.cs
+++ FrontEnd/FrontEnd.Client/obj/Debug/net10.0/FrontEnd.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca6f8d8455480759a5c4d4b54f7d247be7121c6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66eebe1956af12816fa7e48709d119393446d084")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

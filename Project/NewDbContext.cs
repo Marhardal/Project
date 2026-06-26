3c476f6486ba4e956b1c7618f0875e09fedee21e
Martin Harawa <martindharawa@gmail.com>
2026-06-26 16:25:39 +0200

    Deleted NewDBContext

--- Project/NewDbContext.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class NewDbContext(DbContextOptions<NewDbContext> options) : DbContext(options)
-{
-    public DbSet<Project.Models.TasksModel> TasksModel { get; set; } = default!;
-}
